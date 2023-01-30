bfd44effa4e55e2c8ab0cbc98bb9ae6a2f0b49c1
dejesusf <dejesusf@uw.edu>
2023-01-30 10:23:06 -0800

    finished off server.js code

--- server.js
+++ server.js
@@ -1,59 +1,56 @@
 //required packages
 const express= require('express');
 const fs= require ('fs');
-
+const path= require ('path');
 
 const PORT= process.env.PORT || 3000;
-
 const app= express();
 
 //middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api)
-app.use(express.static('public'));
 
-//GET route for notes.html
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-});
+app.use(express.static('/public'));
 
 //GET route for index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'develop/public/index.html'))
+});
+
+//GET route for notes.html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
 });
 
 //GET route for api/notes
 app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'db/db.json'))
+  res.sendFile(path.join(__dirname, 'develop/db/db.json'))
 });
 
 //POST notes to api/notes
 app.post('/api/notes', (req, res) => {
   //log that a POST request was received
-  console.info(`${req.method} request received to add a note`);
-  
   //destructure assignment for the items in req.body
   const { title, text } = req.body;
-
-  //if all the required properties are present
-  if (title && text) {
-    //variable for the object that will save
+  console.info(req.body);
+  
+  fs.readFile('/develop/db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err)
+    }
+    const note= JSON.parse(data)
     const newNote= {
       title,
-      text
+      text,
+      id: note.length++,
     }
-    const response= {
-    status: 'success',
-    body: newNote,
-  };
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in posting note');
-  }
+    note.push(newNote)
+    console.log(note)
+    fs.writeFile('/develop.db/db.json', JSON.stringify(notes), err => console.log(err))
+  })
+  res.json ({ok:true})
 });
 
 app.listen(PORT, () => 
-  console.log(`listenin' on http://localhost:${PORT} ❤️‍🔥`)
+  console.log(`listenin' on http://localhost:${PORT} 🔥`)
 );
